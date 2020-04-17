faa76aad4a81281d2724793a0c3d479b21166c48
Zheng-Luxi <60222167+Zheng-Luxi@users.noreply.github.com>
2020-04-17 16:19:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ canvas.addEventListener('mousemove',function(e){
 function draw(){
 	gd.clearRect( 0, 0, canvas.width, canvas.height );
 	gd.fillStyle = 'rgba( 255, 255, 255, 0.3 )';
+	gd.font = 'italic normal bold 50px Consolas';
 	gd.textAlign = 'center';
 	gd.textBaseline = 'middle';
 	gd.fillText( "Author: Zheng-Luxi", canvas.width / 2, canvas.height / 2 );
